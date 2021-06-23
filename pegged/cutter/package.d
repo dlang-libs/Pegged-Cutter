c8db0c1fba04547d625dcb45333b810bb63e9a48
JavaCommons Technologies <javacommons@gmail.com>
2021-06-24 08:20:27 +0900

    Add: optimize() function

--- pegged/cutter/package.d
+++ pegged/cutter/package.d
@@ -4,6 +4,47 @@ import pegged.grammar;
 
 @safe:
 
+void optimize(TParseTree)(ref TParseTree p, string[] force = null, string[] keep = null)
+{
+	import std.algorithm : canFind, endsWith, startsWith;
+	import std.string : indexOf;
+
+	if (p.children.length == 0)
+		return;
+	bool processed = true;
+	while (processed)
+	{
+		processed = false;
+		ParseTree[] new_children;
+		foreach (ref child; p.children)
+		{
+			if (force !is null && force.canFind(child.name))
+			{
+			}
+			else if (keep !is null && keep.canFind(child.name))
+			{
+				new_children ~= child;
+				continue;
+			}
+			else if (child.children.length != 1)
+			{
+				new_children ~= child;
+				continue;
+			}
+			foreach (ref grand_child; child.children)
+			{
+				new_children ~= grand_child;
+			}
+			processed = true;
+		}
+		p.children = new_children;
+	}
+	foreach (ref child; p.children)
+	{
+		optimize!TParseTree(child, force, keep);
+	}
+}
+
 void cutNodes(TParseTree)(ref TParseTree p, string[] names1 = null, string[] names2 = null)
 {
 	import std.algorithm : canFind, endsWith, startsWith;
